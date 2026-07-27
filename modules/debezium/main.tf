31eb3dddd76245f9553a288c1d53a93538a7d8bd
app-yevgeni <23049337+yevgeni-max2006@users.noreply.github.com>
2026-07-27 10:26:14 +0300

    Update main.tf

--- modules/debezium/main.tf
+++ modules/debezium/main.tf
@@ -1,8 +1,44 @@
-resource "helm_release" "debezium_operator" {
-  name             = "debezium-operator"
-  repository       = "https://charts.debezium.io"
-  chart            = "debezium-operator"
-  version          = "3.2.0-final"
-  namespace        = "debezium"
+
+resource "helm_release" "neuvector" {
+  name             = "neuvector-core"
+  namespace        = "neuvector"
   create_namespace = true
+
+  repository = "https://neuvector.github.io/neuvector-helm/"
+  chart      = "core"
+  version    = "2.8.2"
+
+  values = [yamlencode({
+    rbac = true
+
+    controller = {
+      replicas = 1
+    }
+
+    enforcer = {
+      privileged = true
+
+      # Disable Docker socket usage entirely
+      dockerSock = {
+        enabled = false
+        mountPath = ""
+      }
+
+      # Explicitly enable containerd
+      containerd = {
+        enabled = true
+        sockPath = "/run/k3s/containerd/containerd.sock"  # For K3s
+      }
+
+      # Some versions of NeuVector also respect this field:
+      runtime = "containerd"
+
+      # Optional fallback path override (can help older agents)
+      runtimePath = "/run/k3s/containerd/containerd.sock"
+    }
+
+    scanner = {
+      enabled = true
+    }
+  })]
 }
